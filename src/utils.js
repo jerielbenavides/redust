7cadee8099c1fdd558002fe6b1cab952a268a246
Jeriel Benavides <jerielmanuelb@outlook.com>
2024-11-05 17:05:56 -0500

    Modified 2 second wait to 7 seconds, added phrases

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,20 @@
 /**
  * @function: generateRandomPhrase
  * @description: generates random phrase that can be used to overwrite comments
- * **/
+ **/
 
 export function generateRandomPhrase() {
-  //thanks to http://stackoverflow.com/a/4709034
   var verbs = [
     ["go to", "goes to", "going to", "went to", "gone to"],
     ["look at", "looks at", "looking at", "looked at", "looked at"],
-    ["choose", "chooses", "choosing", "chose", "chosen"]
+    ["choose", "chooses", "choosing", "chose", "chosen"],
+    ["talk about", "talks about", "talking about", "talked about", "talked about"],
+    ["think about", "thinks about", "thinking about", "thought about", "thought about"],
+    ["play with", "plays with", "playing with", "played with", "played with"],
+    ["listen to", "listens to", "listening to", "listened to", "listened to"],
+    ["make", "makes", "making", "made", "made"],
+    ["find", "finds", "finding", "found", "found"],
+    ["create", "creates", "creating", "created", "created"]
   ];
   var tenses = [
     {
@@ -24,7 +30,7 @@ export function generateRandomPhrase() {
       format: "%subject %verb %complement"
     },
     {
-      name: "Present Continues",
+      name: "Present Continuous",
       singular: 2,
       plural: 2,
       format: "%subject %be %verb %complement"
@@ -33,12 +39,21 @@ export function generateRandomPhrase() {
   var subjects = [
     { name: "I", be: "am", singular: 0 },
     { name: "You", be: "are", singular: 0 },
-    { name: "He", be: "is", singular: 1 }
+    { name: "He", be: "is", singular: 1 },
+    { name: "They", be: "are", singular: 0 },
+    { name: "We", be: "are", singular: 0 }
   ];
   var complementsForVerbs = [
-    ["cinema", "park", "home", "concert"],
-    ["for a map", "them", "the stars", "the lake"],
-    ["a book for reading", "a dvd for tonight"]
+    ["cinema", "park", "home", "concert", "the beach"],
+    ["for a map", "them", "the stars", "the lake", "the sunset"],
+    ["a book for reading", "a DVD for tonight", "some music", "a recipe"],
+    ["the news", "my thoughts", "the latest trends", "the weather"],
+    ["the project", "a solution", "the problem", "an idea"],
+    ["my toys", "the game", "the puzzle", "the instrument"],
+    ["the radio", "some advice", "a story", "a podcast"],
+    ["a decision", "a list", "some art", "a meal"],
+    ["my keys", "the perfect gift", "the answer", "the way"],
+    ["a new project", "a plan", "a strategy", "a masterpiece"]
   ];
 
   Array.prototype.random = function() {
@@ -54,10 +69,14 @@ export function generateRandomPhrase() {
     var str = tense.format;
     str = str.replace("%subject", subject.name).replace("%be", subject.be);
     str = str.replace(
-      "%verb",
-      verb[subject.singular ? tense.singular : tense.plural]
+        "%verb",
+        verb[subject.singular ? tense.singular : tense.plural]
     );
     str = str.replace("%complement", complement.random());
+
+    // Append the notice about the Redust extension
+    str += "\n * This comment was anonymized with the r/redust browser extension.";
+
     return str;
   }
 
@@ -65,15 +84,15 @@ export function generateRandomPhrase() {
 }
 
 /**
- * @function resolveAfter2Seconds: Simulates sleep() for 2 seconds and returns a promise.
+ * @function resolveAfter2Seconds: Simulates sleep() for 7 seconds and returns a promise.
  * to be used in conjunction with await.
- * Primary purpose is to respect reddit's API ratelimits (60 per minute)
+ * Primary purpose is to respect reddit's API ratelimits
  */
 export function resolveAfter2Seconds() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve("resolved");
-    }, 2000);
+    }, 7000); //Increased to 7 seconds. Non Oauth = 10 api calls per min.
   });
 }
 
@@ -87,8 +106,8 @@ export function onNetworkError(error) {
     console.log(error.request);
   } else {
     console.log(
-      "An Improper Request was sent to reddit. Please post this on /r/NukeRedditHistory for more help",
-      error.message
+        "An Improper Request was sent to reddit.",
+        error.message
     );
   }
   console.log(error);
